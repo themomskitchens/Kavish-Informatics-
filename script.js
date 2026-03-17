620f70539524fb4299e28a66cb3d517c93ff57da
The Food Corner <manish.tomar841@gmail.com>
2026-03-17 23:56:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,26 @@ update();
 function toggleTheme(){
 document.body.classList.toggle("light");
 }
+
+let btn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.body.scrollTop>300 || document.documentElement.scrollTop>300){
+
+btn.style.display="block";
+
+}else{
+
+btn.style.display="none";
+
+}
+
+};
+
+function topFunction(){
+
+document.body.scrollTop=0;
+document.documentElement.scrollTop=0;
+
+}
